9571acee9f07f07b5f593466f7cfd0bae2dc96ad
yeheskhielprasetyo <yeheskhielprasetyorakordana123@gmail.com>
2023-01-26 11:36:39 -0800

    validate request

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,25 +1,87 @@
 package controllers
 
-func HashPassword() {
+import (
+	"auth-jwt-gin/database"
+	helper "auth-jwt-gin/helpers"
+	"auth-jwt-gin/models"
+	"context"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword()
+
+func VerifyPassword()
+
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "email anda salah"})
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "no hp anda salah"})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "email dan no hp sudah ada"})
+		}
+	}
 }
 
-func VerifyPassword() {
+func Login()
 
-}
+func GetUsers()
 
-func Signup() {
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
 
-}
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-func Login() {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-}
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
 
-func GetUsers() {
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
-}
-
-func GetUser() {
-
+		c.JSON(http.StatusOK, user)
+	}
 }
